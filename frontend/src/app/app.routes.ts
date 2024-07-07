ae27073f7d4312e357d5b306a9c2dd87718556a6
NabilNaou <nabilnaou@hotmail.com>
2024-07-07 17:14:04 +0200

    Set up routing and some package fixes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SecondTabComponent } from './features/second-tab/second-tab.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'second-tab', component: SecondTabComponent },
+  { path: '**', component: HomeComponent },
+];
---
+++ frontend/src/app/features/second-tab/second-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './second-tab.component.html',
+  styleUrl: './second-tab.component.scss'
+})
+export class SecondTabComponent {
+
+}
